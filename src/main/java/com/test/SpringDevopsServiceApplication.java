cc4eaae67d0a5e6e04267115706ac5021894cb21
sandip-sawant <sun.sawant@gmail.com>
2024-07-02 19:46:08 +0530

    Initial commit

---
+++ src/main/java/com/test/SpringDevopsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevopsServiceApplication.class, args);
+	}
+
+}
